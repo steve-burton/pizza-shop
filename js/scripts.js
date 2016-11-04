281068054534e88dd85478034ac2d3bef19626d2
Steve Burton <stevevburton@gmail.com>
2016-11-04 15:17:33 -0700

    add toppings with extra prices

--- js/scripts.js
+++ js/scripts.js
@@ -3,15 +3,7 @@ var Pizza = function(pizzaSize, toppings, cost) {
   this.toppings = toppings;
   this.cost = 20;
 }
-// Pizza.prototype.size = function(){
-//   if(this.pizzaSize = "Large") {
-//     return this.price = 20;
-//   } else if(this.pizzaSize = "Medium") {
-//     return this.price = 17;
-//   } else if(this.pizzaSize = "Small") {
-//     return this.price = 13;
-//   }
-// }
+
 Pizza.prototype.findPrice = function() {
   if(this.pizzaSize === "large") {
     return this.cost;
@@ -22,17 +14,29 @@ Pizza.prototype.findPrice = function() {
   }
 }
 
+Pizza.prototype.findToppings = function() {
+  if(this.toppings === 1) {
+    return 1.5;
+  } else if (this.toppings === 2) {
+    return 3;
+  } else if (this.toppings === 3) {
+    return 4.5;
+  } else {
+    return 6
+  }
+}
+
 
 // User Interface
 $(document).ready(function(){
   $("form.pizza").submit(function(event){
     event.preventDefault();
     var pizzaSize = $("#sizeSelect").val();
+    var toppings = $("[type='checkbox']:checked").length;
     var newPizza = new Pizza(pizzaSize, toppings, cost);
     var cost = newPizza.findPrice();
-    var toppings = $("input#time").val();
     //$("span").text("");
-    $("span#total-price").text(newPizza.findPrice());
+    $("span#total-price").text((newPizza.findPrice()) + (newPizza.findToppings()));
     // document.getElementById("span#total-price").reset("");
   });
 });
