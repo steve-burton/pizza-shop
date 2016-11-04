1b017706bc44997aafaed31eda62864e614d975b
Steve Burton <stevevburton@gmail.com>
2016-11-04 13:00:21 -0700

    trying to change price for different pizza sizes

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,24 @@
-var Pizza = function(size, toppings, cost){
-  this.pizzaSize = size;
+var Pizza = function(pizzaSize, toppings, cost) {
+  this.pizzaSize = pizzaSize;
   this.toppings = toppings;
   this.cost = 20;
 }
-// Pizza.prototype.toppings = function(){
-//   return this.movieName + " <br>at " + this.time;
-// }
-
-// Pizza.prototype.price = function(){
-//   if(this.pizzaSize === "Large") {
+// Pizza.prototype.size = function(){
+//   if(this.pizzaSize = "Large") {
 //     return this.price = 20;
-//   } else if(this.pizzaSize === "Medium") {
+//   } else if(this.pizzaSize = "Medium") {
 //     return this.price = 17;
-//   } else if(this.pizzaSize === "Small") {
+//   } else if(this.pizzaSize = "Small") {
 //     return this.price = 13;
 //   }
 // }
-
-Pizza.prototype.findPrice = function(){
-  if(this.pizzaSize = "Large") {
+Pizza.prototype.findPrice = function(pizzaSize, toppings, cost) {
+  if(this.pizzaSize === "Large") {
     return this.cost;
-  // } else if(this.age <= 13) {
-  //   return this.price - 11;
-  // } else if(this.age >= 65){
-  //   return this.price - 7;
-  // } else if(this.time === "2" || this.time === "12"){
-  //   return this.price - 4;
+  } else if(this.pizzaSize === "Medium") {
+    return 17;
+  } else if(this.pizzaSize === "Small") {
+    return 13;
   // } else {
   //   return this.price;
   }
@@ -33,12 +26,15 @@ Pizza.prototype.findPrice = function(){
 
 // User Interface
 $(document).ready(function(){
-  var newPizza = new Pizza();
+
   $("form.pizza").submit(function(event){
     event.preventDefault();
-    var size = $("#sizeSelect").val();
-    //var toppings = $("input#time").val();
+    var newPizza = new Pizza();
     var pizzaPrice = newPizza.findPrice();
-    $("span#total-price").append(pizzaPrice);
+    var pizzaSize = $("div#size").val();
+    //var toppings = $("input#time").val();
+    $("span").text("");
+    $("span#total-price").text(pizzaPrice);
+    // document.getElementById("span#total-price").reset("");
   });
 });
